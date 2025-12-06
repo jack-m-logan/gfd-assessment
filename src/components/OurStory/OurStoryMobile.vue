2a25867d42111fb602c2afeeff3428359942f829
Jack Logan <jackmlogan92@gmail.com>
2025-12-06 16:20:00 +0000

    bottom left mobile images

--- src/components/OurStory/OurStoryMobile.vue
+++ src/components/OurStory/OurStoryMobile.vue
@@ -2,17 +2,18 @@
 
     <section class="relative min-h-screen bg-bg-secondary overflow-hidden">
         <!-- background white block -->
-        <div class="absolute h-[33%] w-full top-0 left-0 bg-bg-primary">
+        <div class="absolute h-[33%] w-full top-0 left-0 bg-bg-variation">
         </div>
 
         <img src="@/assets/images/optimised/our-story-thatched-cottage.webp"
-            class="absolute top-[20%] left-[6%] w-[42.75rem] max-w-none h-auto grayscale z-10"
+            class="absolute top-[20%] left-[6%] w-[42.75rem] max-w-none h-auto grayscale z-1"
             alt="Exterior of a thatched cottage">
 
         <img src="@/assets/images/optimised/our-story-hero-interior.webp" class="absolute top-3 right-[-10%] w-[16.25rem] 
                   border-6 border-white shadow-xl z-10" alt="Bi-fold doors interior">
 
-        <div class="absolute w-[76%] h-fit-content top-[28%] z-100 left-[2%] bg-bg-primary p-4">
+        <div class="absolute w-[76%] max-w-[20rem] min-w-[12.5rem] 
+                    h-fit-content top-[28%] z-100 left-[2%] bg-bg-primary p-4">
             <h1 class="mb-6 border-b-2 border-red pb-4">Our Story</h1>
 
             <p class="mb-6">
@@ -21,6 +22,20 @@
                 composite doors, and we were one of the first companies in the UK
                 to pioneer online ordering for this product.
             </p>
+        </div>
+
+        <div class="absolute bottom-0 left-[-2%] w-fit-content border-1 border-red-500">
+            <div class="absolute bottom-0 left-0 w-[8.5rem] h-[13.75rem] rotate-357 
+                        border-4 border-white shadow-xl overflow-hidden z-30">
+                <img src="@/assets/images/optimised/our-story-bed-center.webp" class="h-full w-[150%] object-cover"
+                    alt="Bedroom with sky visible through roof light">
+            </div>
+
+            <img src="@/assets/images/optimised/our-story-white-blackout-closed.webp" class="absolute bottom-0 left-[7rem] w-[7.125rem] h-[12.5rem] max-w-none rotate-9
+                border-4 border-white shadow-xl z-10" alt="Bi-fold doors interior">
+
+            <img src="@/assets/images/optimised/our-story-blind-grey.webp" class="absolute bottom-[-2.5rem] left-[9rem] w-[7.125rem] h-[12.5rem] max-w-none rotate-9
+                border-4 border-white shadow-xl z-20" alt="Bi-fold doors interior">
         </div>
     </section>
 </template>
