d09f613516c49869e01a5974d611b10a32ff23c7
Jack Logan <jackmlogan92@gmail.com>
2025-12-08 10:24:20 +0000

    create scalable ImageCollage for image splash

---
+++ src/components/OurStory/ImageCollage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="absolute bottom-0 left-[-2%] w-fit-content origin-bottom-left"
+        :style="{ transform: `scale(${scale})` }">
+
+        <!-- bed/sky light image -->
+        <div class="absolute bottom-0 left-0 w-[8.5rem] h-[13.75rem] rotate-357 
+                border-4 border-white shadow-xl overflow-hidden z-20">
+            <img src="@/assets/images/optimised/our-story-bed-center.webp" class="h-full w-[150%] object-cover"
+                alt="Bedroom with sky visible through roof light">
+        </div>
+
+        <!-- white blind -->
+        <img src="@/assets/images/optimised/our-story-white-blackout-closed.webp" class="absolute bottom-[-3rem] left-[7rem] w-[7.125rem] h-[12.5rem] max-w-none rotate-9
+                border-4 border-white shadow-xl z-10" alt="White blackout blind">
+
+        <!-- grey blind -->
+        <img src="@/assets/images/optimised/our-story-blind-grey.webp" class="absolute bottom-[-6.5rem] left-[9rem] w-[7.125rem] h-[12.5rem] max-w-none rotate-9
+                border-4 border-white shadow-xl z-30" alt="Grey iBlind">
+
+        <!-- antique oak -->
+        <img src="@/assets/images/optimised/our-story-antique-oak-seatch.webp" class="absolute bottom-[-6rem] left-[-4rem] w-[14.5rem] h-auto max-w-none rotate-252
+                border-4 border-white shadow-xl z-10" alt="Antique oak door material">
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    scale: {
+        type: Number,
+        default: 1
+    }
+})
+</script>
--- src/components/OurStory/OurStoryMobile.vue
+++ src/components/OurStory/OurStoryMobile.vue
@@ -1,15 +1,18 @@
 <template>
-    <section class="relative min-h-screen bg-bg-secondary">
+    <!-- Our Story -->
+    <section class="relative min-h-screen bg-bg-secondary overflow-x-clip">
         <!-- background white block -->
         <div class="absolute h-[33%] w-full top-0 left-0 bg-bg-variation">
         </div>
 
-        <img src="@/assets/images/optimised/our-story-thatched-cottage.webp"
-            class="absolute top-[20%] left-[6%] w-[42.75rem] max-w-none h-auto grayscale z-1"
-            alt="Exterior of a thatched cottage">
+        <div class="absolute top-[20%] left-[6%] overflow-x-hidden">
+            <img src="@/assets/images/optimised/our-story-thatched-cottage.webp"
+                class="w-[42.75rem] max-w-none h-auto grayscale z-1" alt="Exterior of a thatched cottage">
+        </div>
 
-        <img src="@/assets/images/optimised/our-story-hero-interior.webp" class="absolute top-3 right-[-10%] w-[16.25rem] 
-                  border-6 border-white shadow-xl z-10" alt="Bi-fold doors interior">
+        <img src="@/assets/images/optimised/our-story-hero-interior.webp"
+            class="absolute top-3 right-[-10%] w-[16.25rem] border-6 border-white shadow-xl z-10"
+            alt="Bi-fold doors interior">
 
         <div class="absolute w-[76%] max-w-[20rem] min-w-[12.5rem] 
                     h-fit-content top-[28%] z-100 left-[2%] bg-bg-primary p-4">
@@ -23,28 +26,10 @@
             </p>
         </div>
 
-        <div class="absolute bottom-0 left-[-2%] w-fit-content border-1 border-red-500">
-            <!-- bed/sky light image, contained within a div to allow overflow hidden -->
-            <div class="absolute bottom-0 left-0 w-[8.5rem] h-[13.75rem] rotate-357 
-                        border-4 border-white shadow-xl overflow-hidden z-20">
-                <img src="@/assets/images/optimised/our-story-bed-center.webp" class="h-full w-[150%] object-cover"
-                    alt="Bedroom with sky visible through roof light">
-            </div>
-
-            <!-- white blind -->
-            <img src="@/assets/images/optimised/our-story-white-blackout-closed.webp" class="absolute bottom-[-3rem] left-[7rem] w-[7.125rem] h-[12.5rem] max-w-none rotate-9
-                border-4 border-white shadow-xl z-10" alt="While blackout blind">
-
-            <!-- grey blind -->
-            <img src="@/assets/images/optimised/our-story-blind-grey.webp" class="absolute bottom-[-6.5rem] left-[9rem] w-[7.125rem] h-[12.5rem] max-w-none rotate-9
-                border-4 border-white shadow-xl z-30" alt="Grey iBlind">
-
-            <!-- antique oak img -->
-            <img src="@/assets/images/optimised/our-story-antique-oak-seatch.webp" class="absolute bottom-[-6rem] left-[-4rem] w-[14.5rem] h-auto max-w-none rotate-252
-                border-4 border-white shadow-xl z-10" alt="Antique oak door material">
-        </div>
+        <ImageCollage :scale="1" class="z-50 absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2" />
     </section>
 
+    <!-- Our Story continued -->
     <section class="relative min-h-screen bg-bg-primary overflow-hidden">
         <div class="absolute w-[92%] max-w-[20rem] min-w-[12.5rem] 
                     h-fit-content top-[28%] z-50 right-0 bg-bg-variation p-4">
@@ -57,14 +42,16 @@
             </p>
         </div>
 
-        <img src="@/assets/images/optimised/our-story-office-building.webp" class="absolute bottom-[5rem] right-0 w-auto h-[15.5rem] max-w-none rotate-5
-                border-4 border-white shadow-xl grayscale z-10" alt="Office building exterior">
+        <img src="@/assets/images/optimised/our-story-office-building.webp"
+            class="absolute bottom-[5rem] right-0 w-auto h-[15.5rem] max-w-none rotate-5 border-4 border-white shadow-xl grayscale z-10"
+            alt="Office building exterior">
 
-        <img src="@/assets/images/optimised/our-story-green-door.webp" class="absolute bottom-[8rem] right-[6rem] w-auto h-[16rem] max-w-none rotate-5
-                border-4 border-white shadow-xl z-100" alt="Office building exterior">
+        <img src="@/assets/images/optimised/our-story-green-door.webp"
+            class="absolute bottom-[8rem] right-[6rem] w-auto h-[16rem] max-w-none rotate-5 border-4 border-white shadow-xl z-100"
+            alt="Office building exterior">
     </section>
 </template>
 
 <script setup lang="ts">
-
+import ImageCollage from './ImageCollage.vue';
 </script>
