001e5b5c15c332889a1d28474a7191374c6b8ff2
Jack Logan <jackmlogan92@gmail.com>
2025-12-09 12:33:09 +0000

    fix up modal styles

--- src/components/TeamGallery/TeamMemberModal.vue
+++ src/components/TeamGallery/TeamMemberModal.vue
@@ -3,30 +3,37 @@
         class="fixed inset-0 z-[100] bg-opacity-70 flex items-center justify-center transition-opacity duration-300">
 
         <div @click.stop
-            class="bg-white p-6 w-full h-full md:w-1/2 md:h-auto md:max-w-lg md:rounded-lg overflow-y-auto">
+            class="bg-white p-6 w-full h-full md:w-4/5 md:max-h-[95vh] md:max-w-[90vw] md:rounded-lg border-1 border-gray-300 shadow-xl overflow-y-auto">
 
             <div class="flex justify-between items-center border-b mb-4">
-                <h3 class="text-xl"><span class="font-bold">Meet, </span>{{ member.name }}!</h3>
-                <button @click="close" class="text-3xl text-gray-500 hover:text-black">&times;</button>
+                <h2>Meet, <span class="font-normal">{{ member.name }}!</span></h2>
+                <button @click="close" class="text-4xl text-gray-500 hover:text-black">&times;</button>
             </div>
 
-            <PaginationControls class="mt-6 md:hidden   " @previous="close" @next="close" />
+            <PaginationControls class="mt-6" @previous="close" @next="close" />
 
-            <div class="md:flex md:space-x-4">
-                <div class="mb-4 md:mb-0 md:w-2/5 flex-shrink-0 flex items-center justify-center bg-gray-200 shadow-lg">
+            <div class="max-w-md mx-auto">
+                <div class="mb-6 mt-6 flex justify-center">
+                    <div class="relative w-40 h-auto md:w-48 md:h-auto pt-4">
+                        <div
+                            class="absolute inset-0 bg-bg-secondary z-0 transform -translate-x-2 -translate-y-2 md:-translate-x-4 md:-translate-y-4">
+                        </div>
 
-                    <img v-if="!modalImageLoadError" :src="member.photoUrlColor" :alt="member.name"
-                        class="w-full h-auto object-cover" @error="handleModalImageError">
+                        <div
+                            class="relative w-full h-full z-10 overflow-hidden shadow-lg transform translate-x-2 translate-y-2 md:translate-x-4 md:translate-y-4">
+                            <img v-if="!modalImageLoadError" :src="member.photoUrlColor" :alt="member.name"
+                                class="w-full h-full object-cover" @error="handleModalImageError">
 
-                    <!-- would typically add a placeholder image or improved visual here -->
-                    <div v-else class="text-center p-8">
-                        <p class="text-sm mt-2 text-gray-500">Photo unavailable.</p>
+                            <div v-else class="w-full h-full flex items-center justify-center bg-gray-300">
+                                <p>Photo unavailable.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="md:w-3/5">
-                    <p class="font-bold text-lg pb-1 mb-2">{{ member.title }}</p>
-                    <p class="text-gray-700 text-sm">{{ member.bio }}</p>
+                <div class="pt-2">
+                    <h2 class="border-b pb-1 mb-2">{{ member.title }}</h2>
+                    <p class="text-justify">{{ member.bio }}</p>
                     <PaginationControls class="hidden mt-6" @previous="close" @next="close" />
                 </div>
             </div>
