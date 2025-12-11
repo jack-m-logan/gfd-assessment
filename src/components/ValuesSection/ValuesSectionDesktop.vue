2bc32987272f617d8d7e3831d2388e1bc72e704e
Jack Logan <jackmlogan92@gmail.com>
2025-12-11 22:29:44 +0000

    values section desktop add images and adjustments

--- src/components/ValuesSection/ValuesSectionDesktop.vue
+++ src/components/ValuesSection/ValuesSectionDesktop.vue
@@ -1,24 +1,32 @@
 <template>
-    <section
-        class="relative bg-bg-variation overflow-hidden hidden md:block py-20 md:h-screen md:min-h-[800px] md:max-h-[1000px]">
+    <section class="relative bg-bg-variation overflow-hidden hidden md:block pt-20">
         <div class="absolute h-full w-full top-0 left-0">
-            <div class="h-[45%] shadow-xl bg-bg-primary border-2 border-blue-500"></div>
+            <div class="h-[45%] shadow-xl bg-bg-primary"></div>
         </div>
 
-        <div class="relative w-full h-full max-w-[1440px] mx-auto px-4 overflow-x-clip border-2 border-red-500">
-
+        <div class="relative w-full max-w-[1440px] mx-auto px-4 overflow-x-clip">
             <div class="grid grid-cols-12 gap-8">
-                <div class="col-span-12 md:col-span-4 border-2 border-green-500">
+                <div class="relative col-span-12 md:col-span-4">
                     <div class="grid grid-cols-2 gap-2">
-                        <div class="w-full h-full border-2 border-purple-500 col-span-1 md:col-start-2 lg:col-start-1">
+                        <div class="w-full col-span-1 md:col-start-2 lg:col-start-1">
                             <h2 class="mb-0 whitespace-nowrap">Company<br> Values & Ethos</h2>
+                        </div>
+
+                        <OfficeImages
+                            class="absolute md:top-[10rem] lg:top-[15rem] md:right-[20%] lg:right-[6rem] z-10" />
+
+                        <div
+                            class="absolute bottom-0 left-1/2 -translate-x-1/2 h-[200px] w-[160px] md:w-[200px] overflow-visible">
+                            <img src="@/assets/images/optimised/values-ethos-cottage.webp"
+                                class="w-full h-full object-cover object-center shadow-xl"
+                                alt="Exterior of a cottage with a blue door">
                         </div>
                     </div>
                 </div>
 
-                <div class="col-span-12 md:col-span-8 shadow-xl p-8 border-2 border-purple-500">
-                    <div class="grid grid-rows-3 gap-4">
-                        <div class="row-span-1 h-[16vh] min-h-[150px] max-h-[200px] border-2 border-yellow-500">
+                <div class="col-span-12 md:col-span-8 p-8">
+                    <div class="flex flex-col gap-4">
+                        <div class="h-[150px] md:h-[180px] lg:h-[200px]">
                             <div class="flex flex-row h-full">
                                 <img src="@/assets/images/optimised/values-ethos-first-row-1.webp"
                                     class="h-full w-auto object-cover shadow-xl" alt="Exterior of a home">
@@ -32,7 +40,7 @@
                                     class="h-full w-auto object-cover shadow-xl" alt="Exterior of a home">
                             </div>
                         </div>
-                        <div class="row-span-1 h-[19.5vh] min-h-[175px] max-h-[220px] border-2 border-yellow-500">
+                        <div class="h-[175px] md:h-[200px] lg:h-[220px]">
                             <div class="flex flex-row h-full">
                                 <img src="@/assets/images/optimised/values-ethos-second-row-1.webp"
                                     class="h-full w-auto object-cover shadow-xl" alt="Exterior of a home">
@@ -44,7 +52,7 @@
                                     class="h-full w-auto object-cover shadow-xl" alt="Exterior of a home">
                             </div>
                         </div>
-                        <div class="row-span-1 border-2 border-pink-500 flex items-center justify-center">
+                        <div class="flex items-center justify-center py-8 border-b-1">
                             <div>
                                 <p class="mb-4">We know that our continued success is down to our customers, and
                                     treating them with
@@ -52,7 +60,7 @@
                                     equally, regardless of the order size.</p>
                                 <p>As the world becomes more automated by the day, we promise never to lose the personal
                                     touch with our customers that we built the foundations of the business on. Our staff
-                                    are friendly, knowledgeable and always happy to assist. </p>
+                                    are friendly, knowledgeable and always happy to assist. </p>
                             </div>
                         </div>
                     </div>
@@ -61,3 +69,7 @@
         </div>
     </section>
 </template>
+
+<script setup lang="ts">
+import OfficeImages from '../OurStory/OfficeImages.vue';
+</script>
